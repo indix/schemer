ab5afb42f1312959f99c36c9859fbc01f16bb9a9
Manoj <manojlds@gmail.com>
2017-11-29 15:58:00 +0530

    Add helper for pretty json

--- schemer-core/src/main/scala/schemer/utils/JSONUtil.scala
+++ schemer-core/src/main/scala/schemer/utils/JSONUtil.scala
@@ -1,6 +1,6 @@
 package schemer.utils
 
-import com.fasterxml.jackson.databind.ObjectMapper
+import com.fasterxml.jackson.databind.{ObjectMapper, SerializationFeature}
 import com.fasterxml.jackson.module.scala.DefaultScalaModule
 import com.fasterxml.jackson.annotation.JsonInclude.Include
 
@@ -14,6 +14,8 @@ private[schemer] object JSONUtil {
   mapper.setSerializationInclusion(Include.NON_NULL)
 
   def toJson(value: Any) = mapper.writeValueAsString(value)
+
+  def prettyJson(value: Any) = mapper.enable(SerializationFeature.INDENT_OUTPUT).writeValueAsString(value)
 
   def fromJson[T: ClassTag](json: String) = {
     val classType = implicitly[ClassTag[T]].runtimeClass.asInstanceOf[Class[T]]
