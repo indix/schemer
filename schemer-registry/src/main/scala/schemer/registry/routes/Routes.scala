38a08728335131fee40928399613c2f38072460e
Manoj <manojlds@gmail.com>
2017-12-11 14:56:20 +0530

    Not log health check requests

--- schemer-registry/src/main/scala/schemer/registry/routes/Routes.scala
+++ schemer-registry/src/main/scala/schemer/registry/routes/Routes.scala
@@ -1,6 +1,6 @@
 package schemer.registry.routes
 
-import akka.http.scaladsl.model.StatusCodes
+import akka.http.scaladsl.model.{StatusCodes, Uri}
 import akka.http.scaladsl.server.Directives._
 import akka.http.scaladsl.server.{ExceptionHandler, RejectionHandler}
 import com.typesafe.scalalogging.StrictLogging
@@ -11,12 +11,19 @@ trait Routes extends GraphQLRoutes with HealthRoutes with StrictLogging {
       logger.error(s"Exception during client request processing: ${e.getMessage}", e)
       _.complete((StatusCodes.InternalServerError, "Internal server error"))
   }
-  val rejectionHandler = RejectionHandler.default
+  val rejectionHandler  = RejectionHandler.default
+  val logBlackListPaths = Seq("health")
+  private def isBlacklistedPath(uri: Uri) =
+    logBlackListPaths
+      .map(s"/" + _)
+      .exists(uri.toString().contains)
   val logDuration = extractRequestContext.flatMap { ctx =>
     val start = System.currentTimeMillis()
     mapResponse { resp =>
       val d = System.currentTimeMillis() - start
-      logger.info(s"[${resp.status.intValue()}] ${ctx.request.method.name} ${ctx.request.uri} took: ${d}ms")
+      if (!isBlacklistedPath(ctx.request.uri)) {
+        logger.info(s"[${resp.status.intValue()}] ${ctx.request.method.name} ${ctx.request.uri} took: ${d}ms")
+      }
       resp
     } & handleRejections(rejectionHandler)
   }
