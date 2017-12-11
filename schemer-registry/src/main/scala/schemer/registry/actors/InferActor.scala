ee7571b2366a741931ce7e4c638f05e0edd778e6
Manoj <manojlds@gmail.com>
2017-12-11 23:04:45 +0530

    Fix the dead letters issue

--- schemer-registry/src/main/scala/schemer/registry/actors/InferActor.scala
+++ schemer-registry/src/main/scala/schemer/registry/actors/InferActor.scala
@@ -1,6 +1,6 @@
 package schemer.registry.actors
 
-import akka.actor.{Actor, ActorSystem}
+import akka.actor.{Actor, ActorRef, ActorSystem}
 import com.typesafe.scalalogging.StrictLogging
 import org.apache.spark.sql.SparkSession
 import schemer.{AvroSchema, CSVSchema, JSONSchema, ParquetSchema}
@@ -11,18 +11,20 @@ import scala.util.{Failure, Success, Try}
 class InferActor(implicit val spark: SparkSession, implicit val system: ActorSystem) extends Actor with StrictLogging {
 
   def receive = {
-    case JSONSchemaInferenceRequest(paths)         => handleException { JSONSchema().infer(paths: _*) }
-    case AvroSchemaInferenceRequest(paths)         => handleException { AvroSchema().infer(paths: _*) }
-    case CSVSchemaInferenceRequest(options, paths) => handleException { CSVSchema(options).infer(paths: _*) }
-    case ParquetSchemaInferenceRequest(t, paths)   => handleException { ParquetSchema(t).infer(paths: _*) }
+    case JSONSchemaInferenceRequest(paths)         => handleException(sender()) { JSONSchema().infer(paths: _*) }
+    case AvroSchemaInferenceRequest(paths)         => handleException(sender()) { AvroSchema().infer(paths: _*) }
+    case CSVSchemaInferenceRequest(options, paths) => handleException(sender()) { CSVSchema(options).infer(paths: _*) }
+    case ParquetSchemaInferenceRequest(t, paths)   => handleException(sender()) { ParquetSchema(t).infer(paths: _*) }
     case _                                         => logger.info("Unsupported infer request")
   }
 
-  def handleException(block: => Any) = Try(block) match {
-    case Success(result) => sender() ! result
+  def handleException(sender: ActorRef)(block: => Any) = Try(block) match {
+    case Success(result) =>
+      logger.info("Sending back inference result")
+      sender ! result
     case Failure(e) =>
       val message = s"Error while trying to infer schema - ${e.getMessage}"
       logger.info(message)
-      sender() ! akka.actor.Status.Failure(new SchemerInferenceException(message))
+      sender ! akka.actor.Status.Failure(new SchemerInferenceException(message))
   }
 }
