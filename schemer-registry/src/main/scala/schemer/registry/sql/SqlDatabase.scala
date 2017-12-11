6aefed1003570e6e48fd5f18ce9bb8032f1bff76
Manoj <manojlds@gmail.com>
2017-12-11 13:15:02 +0530

    Ability to override postgres url

--- schemer-registry/src/main/scala/schemer/registry/sql/SqlDatabase.scala
+++ schemer-registry/src/main/scala/schemer/registry/sql/SqlDatabase.scala
@@ -1,15 +1,19 @@
 package schemer.registry.sql
 
 import io.getquill.{PostgresAsyncContext, SnakeCase}
+import org.apache.commons.lang3.StringUtils
 import org.flywaydb.core.Flyway
 
 case class SqlDatabase(config: DatabaseConfig) {
   lazy val ctx = new PostgresAsyncContext(SnakeCase, config.postgresConfig)
 
   def updateSchema() = {
-    val flyway = new Flyway()
-    flyway.setOutOfOrder(true)
-    flyway.setDataSource(s"jdbc:${config.postgresConfig.getString("url")}", "", "")
-    flyway.migrate()
+    val postgresUrl = config.postgresConfig.getString("url")
+    if (StringUtils.isNotEmpty(postgresUrl)) {
+      val flyway = new Flyway()
+      flyway.setOutOfOrder(true)
+      flyway.setDataSource(s"jdbc:$postgresUrl", "", "")
+      flyway.migrate()
+    }
   }
 }
