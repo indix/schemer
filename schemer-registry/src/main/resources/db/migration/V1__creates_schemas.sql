4b39c0d9973e3d2e85361c1b732ce0d666d6e926
Manoj <manojlds@gmail.com>
2017-12-19 16:22:59 +0530

    Updated schema and namespace creation

--- schemer-registry/src/main/resources/db/migration/V1__creates_schemas.sql
+++ schemer-registry/src/main/resources/db/migration/V1__creates_schemas.sql
@@ -1,6 +1,17 @@
--- SchemaRegistry
+CREATE EXTENSION IF NOT EXISTS "uuid-ossp";
+
+CREATE TABLE "namespaces"(
+    "id" UUID NOT NULL DEFAULT uuid_generate_v4(),
+    "name" VARCHAR NOT NULL
+);
+
+ALTER TABLE "namespaces" ADD CONSTRAINT "namespaces_id" PRIMARY KEY("id");
+CREATE UNIQUE INDEX "namespaces_name" ON "namespaces"("name");
+
+INSERT INTO "namespaces"("name") VALUES('default');
+
 CREATE TABLE "schemas"(
-    "id" UUID NOT NULL,
+    "id" UUID NOT NULL DEFAULT uuid_generate_v4(),
     "name" VARCHAR NOT NULL,
     "namespace" VARCHAR NOT NULL,
     "type" VARCHAR NOT NULL,
@@ -8,4 +19,7 @@ CREATE TABLE "schemas"(
     "created_by" VARCHAR NOT NULL
 
 );
+
 ALTER TABLE "schemas" ADD CONSTRAINT "schemas_id" PRIMARY KEY("id");
+CREATE UNIQUE INDEX "schemas_name_namespace" ON "schemas"("name","namespace");
+ALTER TABLE "schemas" ADD CONSTRAINT "schemas_namespace_fk" FOREIGN KEY("namespace") REFERENCES "namespaces"("name");
