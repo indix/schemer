0ce7e446390cd62a602fc4d1ac350341a2e6d4db
Manoj <manojlds@gmail.com>
2018-03-02 15:51:44 +0530

    Push latest tag too

--- publish.sh
+++ publish.sh
@@ -8,3 +8,5 @@ sbt sonatypeReleaseAll
 docker login -u "$DOCKER_USERNAME" -p "$DOCKER_PASSWORD"
 sbt docker:publishLocal
 docker push indix/schemer-registry:${TRAVIS_TAG}
+docker tag indix/schemer-registry:${TRAVIS_TAG} indix/schemer-registry:latest
+docker push indix/schemer-registry:latest
